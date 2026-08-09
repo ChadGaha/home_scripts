b95137b95b2e2262eadd027c3c4e04ad32e1b7f6
Chad Gahafer <chad.gahafer@gmail.com>
2026-08-09 07:53:40 -0400

    named the google sheet correctly

--- task_uploader.py
+++ task_uploader.py
@@ -24,7 +24,7 @@ creds = Credentials.from_service_account_file(
 client = gspread.authorize(creds)
 
 # Open the Google Sheet by title
-sheet = client.open("Todolist").worksheet("Shared") # Or use sheet index/URL
+sheet = client.open("Todolist <3").worksheet("Shared") # Or use sheet index/URL
 
 all_current_tasks = sheet.get_all_values()
 current_tasks = list()
@@ -58,7 +58,7 @@ if len(daily_tasks) > 0:
 else:
     print('No daily tasks to upload')
 
-completed_sheet = client.open("Todolist").worksheet("Completed Tasks")
+completed_sheet = client.open("Todolist <3").worksheet("Completed Tasks")
 sheet_data = completed_sheet.get_all_values()
 
 headers = sheet_data[0]
