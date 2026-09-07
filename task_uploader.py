c6da137a6d57c5267329de8fb6b71abbc518abc2
Chad Gahafer <chad.gahafer@gmail.com>
2026-09-07 16:55:41 -0400

    Made it match the current production script, but with secret.json called instead

--- task_uploader.py
+++ task_uploader.py
@@ -6,12 +6,6 @@ from google.oauth2.service_account import Credentials
 from datetime import datetime, timedelta
 import pandas as pd
 
-dt = datetime.now().strftime("%m/%d/%y %H:%M:%S")
-
-daily_tasks = [
-    ["Make the bed (Daily)", 3, dt]
-]
-    
 
 # Define the scope (permissions you need)
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
@@ -20,8 +14,17 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 creds = Credentials.from_service_account_file(
     'secrets.json', scopes=scope)
 
+raw_date = datetime.now()
+dt = raw_date.strftime("%m/%d/%y %H:%M:%S")
+
+daily_tasks = [
+    ["Make the bed (Daily)", 3, dt]
+]
+
 # Authenticate and create the client
 client = gspread.authorize(creds)
+
+
 
 # Open the Google Sheet by title
 
@@ -46,6 +49,7 @@ rule = DataValidationRule(
 )
 
 daily_tasks = [task for task in daily_tasks if task[0] not in current_tasks]
+daily_length = len(daily_tasks)
 
 for i, task in enumerate(daily_tasks):
     # Add an empty string for the checkbox column
@@ -59,8 +63,8 @@ for i, task in enumerate(daily_tasks):
     set_data_validation_for_cell_range(sheet, f'D{current_row}', rule)
 
 #print (type(rule))
-if len(daily_tasks) > 0:
-    print('Upload of daily tasks successful.')
+if daily_length  > 0:
+    print('Daily tasks uploaded:', daily_length)
 else:
     print('No daily tasks to upload')
 
@@ -77,20 +81,30 @@ completed_df = completed_df.dropna(subset=["Date Finished"])
 
 
 weekly_tasks = [
-    ["Move trash can to curb (Weekly)", 2, dt]
+    
+    ["Mop floor (Weekly)", 2, dt]
 
 ]
 
-week_cutoff_date = datetime.now() - timedelta(days=7)
+Monday_tasks = [
+    ["Move trash can to curb (Weekly)", 2, dt]
+]
 
+week_cutoff_date = datetime.now() - timedelta(days=6)
 
 last_week_df = completed_df[completed_df["Date Finished"] > week_cutoff_date]
 last_week_list = last_week_df["Task"].tolist()
 #print(last_week_list)
 
+weekly_tasks = [task for task in weekly_tasks if task[0] not in last_week_list]
+
+if raw_date.weekday() == 0:
+    for task in Monday_tasks:
+        weekly_tasks.append(task)
+
 weekly_tasks = [task for task in weekly_tasks if task[0] not in current_tasks]
 
-weekly_tasks = [task for task in weekly_tasks if task[0] not in last_week_list]
+weekly_length = len(weekly_tasks)
 
 starting_row = len(sheet.get_all_values()) + 1
 
@@ -105,8 +119,8 @@ for i, task in enumerate(weekly_tasks):
     # Add checkbox to column D (4)
     set_data_validation_for_cell_range(sheet, f'D{current_row}', rule)
 
-if len(weekly_tasks) > 0:
-    print('Uploaded weekly tasks.')
+if weekly_length > 0:
+    print('Weekly tasks uploaded:', weekly_length)
 else:
     print('No weekly tasks to upload.')
 
@@ -114,7 +128,7 @@ biweekly_tasks = [
     ["Wash bed sheets (Biweekly)", 2, dt]
 ]
 
-biweek_cutoff_date = datetime.now() - timedelta(days=14)
+biweek_cutoff_date = datetime.now() - timedelta(days=13)
 
 biweek_df = completed_df[completed_df["Date Finished"] >= biweek_cutoff_date]
 
@@ -123,6 +137,8 @@ biweek_list = biweek_df["Task"].tolist()
 biweekly_tasks = [task for task in biweekly_tasks if task[0] not in current_tasks]
 
 biweekly_tasks = [task for task in biweekly_tasks if task[0] not in biweek_list]
+
+biweekly_length = len(biweekly_tasks)
 
 starting_row = len(sheet.get_all_values()) + 1
 
@@ -137,16 +153,16 @@ for i, task in enumerate(biweekly_tasks):
     # Add checkbox to column D (4)
     set_data_validation_for_cell_range(sheet, f'D{current_row}', rule)
 
-if len(biweekly_tasks) > 0:
-    print('Uploaded biweekly tasks.')
+if biweekly_length > 0:
+    print('Biweekly tasks uploaded:', biweekly_length)
 else:
     print("No biweekly tasks to upload.")
 
 monthly_tasks = [
-    ["Wash car exterior, vacuum interior (Monthly)", 1, dt]
+    ["Organize pantry (Monthly)", 1, dt]
 ]
 
-month_cutoff_date = datetime.now() - timedelta(days = 30)
+month_cutoff_date = datetime.now() - timedelta(days = 28)
 
 month_df = completed_df[completed_df["Date Finished"] >= month_cutoff_date]
 month_list = month_df["Task"].tolist()
@@ -154,6 +170,8 @@ month_list = month_df["Task"].tolist()
 monthly_tasks = [task for task in monthly_tasks if task[0] not in current_tasks]
 
 monthly_tasks = [task for task in monthly_tasks if task[0] not in month_list]
+
+monthly_length = len(monthly_tasks)
 
 starting_row = len(sheet.get_all_values()) + 1
 
@@ -168,8 +186,8 @@ for i, task in enumerate(monthly_tasks):
     # Add checkbox to column D (4)
     set_data_validation_for_cell_range(sheet, f'D{current_row}', rule)
 
-if len(monthly_tasks) > 0:
-    print('Uploaded monthly tasks')
+if monthly_length > 0:
+    print('Monthly taks uploaded:', monthly_length)
 else:
     print("No monthly taks to upload")
 
@@ -186,6 +204,8 @@ quarterly_tasks = [task for task in quarterly_tasks if task[0] not in current_ta
 
 quarterly_tasks = [task for task in quarterly_tasks if task[0] not in quarter_list]
 
+quarterly_length = len(quarterly_tasks)
+
 starting_row = len(sheet.get_all_values()) + 1
 
 for i, task in enumerate(quarterly_tasks):
@@ -199,7 +219,118 @@ for i, task in enumerate(quarterly_tasks):
     # Add checkbox to column D (4)
     set_data_validation_for_cell_range(sheet, f'D{current_row}', rule)
 
-if len(quarterly_tasks) > 0:
-    print('Uploaded quarterly tasks.')
+if quarterly_length > 0:
+    print('Quarterly tasks uploaded:', quarterly_length)
 else:
     print("No quarterly tasks to upload today.")
+
+
+print("\n")
+print("It is time to upload tasks for Lydia\n")
+#Lyd's tasks
+
+Lyd_sheet = client.open("Todolist <3").worksheet("Lydia") # Or use sheet index/URL
+
+all_Lyd_current_tasks = Lyd_sheet.get_all_values()
+Lyd_current_tasks = list()
+for t in all_Lyd_current_tasks:
+    Lyd_current_tasks.append(t[0])
+
+Lyd_starting_row = len(Lyd_sheet.get_all_values()) + 1
+
+Lyd_daily_tasks = [
+    ["Brush teeth morning (Daily)", 3, dt]
+]
+
+
+Lyd_daily_tasks = [task for task in Lyd_daily_tasks if task[0] not in Lyd_current_tasks]
+Lyd_daily_length = len(Lyd_daily_tasks)
+
+for i, task in enumerate(Lyd_daily_tasks):
+    # Add an empty string for the checkbox column
+    task.append("")
+    Lyd_sheet.append_row(task)
+    #print(task)
+    # Figure out which row this is
+    current_row = Lyd_starting_row + i
+
+    # Add checkbox to column D (4)
+    set_data_validation_for_cell_range(Lyd_sheet, f'D{current_row}', rule)
+
+if Lyd_daily_length > 0:
+    print('Total daily tasks for Lydia uploaded:', Lyd_daily_length)
+else:
+    print("No daily tasks for Lydia to upload today.")
+
+
+Lyd_weekly_tasks = [
+    ["Play piano (Weekly)", 1, dt]
+]
+
+Lyd_weekly_tasks = [task for task in Lyd_weekly_tasks if task[0] not in Lyd_current_tasks]
+
+Lyd_weekly_tasks = [task for task in Lyd_weekly_tasks if task[0] not in last_week_list]
+Lyd_weekly_length = len(Lyd_weekly_tasks)
+
+Lyd_starting_row = len(Lyd_sheet.get_all_values()) + 1
+
+for i, task in enumerate(Lyd_weekly_tasks):
+    # Add an empty string for the checkbox column
+    task.append("")
+    Lyd_sheet.append_row(task)
+    #print(task)
+    # Figure out which row this is
+    current_row = Lyd_starting_row + i
+
+    # Add checkbox to column D (4)
+    set_data_validation_for_cell_range(Lyd_sheet, f'D{current_row}', rule)
+
+if Lyd_weekly_length > 0:
+    print('Total weekly tasks for Lydia uploaded:', Lyd_weekly_length)
+else:
+    print("No weekly tasks for Lydia to upload today.")
+
+print("\n")
+print("It is time to upload tasks for Chad\n")
+
+Chad_sheet = client.open("Todolist <3").worksheet("Chad") # Or use sheet index/URL
+
+all_Chad_current_tasks = Chad_sheet.get_all_values()
+Chad_current_tasks = list()
+for t in all_Chad_current_tasks:
+    Chad_current_tasks.append(t[0])
+
+Chad_starting_row = len(Chad_sheet.get_all_values()) + 1
+
+Chad_daily_tasks = [
+    ["Brush teeth in morning (Daily)", 3, dt]
+]
+
+Chad_daily_work_tasks = [
+    ["Active in Omni (Work)", 1, dt]
+]
+
+
+if raw_date.weekday() < 5:
+    for task in Chad_daily_work_tasks:
+        Chad_daily_tasks.append(task)
+
+
+Chad_daily_tasks = [task for task in Chad_daily_tasks if task[0] not in Chad_current_tasks]
+Chad_daily_length = len(Chad_daily_tasks)
+
+for i, task in enumerate(Chad_daily_tasks):
+    # Add an empty string for the checkbox column
+    task.append("")
+    Chad_sheet.append_row(task)
+    #print(task)
+    # Figure out which row this is
+    current_row = Chad_starting_row + i
+
+    # Add checkbox to column D (4)
+    set_data_validation_for_cell_range(Chad_sheet, f'D{current_row}', rule)
+
+if Chad_daily_length > 0:
+    print('Total daily tasks for Chad uploaded:', Chad_daily_length)
+else:
+    print("No daily tasks for Chad to upload today.")
